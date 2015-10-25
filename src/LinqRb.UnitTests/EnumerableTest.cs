947c650ad0c8e1c1eb97ef579b711fcf45d1367a
Tommy Parnell <tparnell8@gmail.com>
2015-10-24 20:38:43 -0400

    init

---
+++ src/LinqRb.UnitTests/EnumerableTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LinqRb.UnitTests
+{
+	public class EnumerableTest
+	{
+		[Fact]
+		public void TestRejectSuccessful()
+		{
+			var range = Enumerable.Range(1, 5);
+			var rejected = range.Reject(a => a == 3);
+			foreach(var number in rejected)
+			{
+				Assert.NotEqual(number, 3);
+			}
+			Assert.True(rejected.Count() == 4);
+		}
+
+		[Fact]
+		public void TestAssoc()
+		{
+			var outer = new List<List<string>>();
+			outer.Add(new List<string>() { "books", "school", "apple", "playtime" });
+			outer.Add(new List<string>() { "star", "wars", "vader" });
+			var assc = outer.AssocFirstOrDefault("wars");
+			Assert.True(assc.Count() == 3);
+			var assc2 = outer.AssocFirstOrDefault("poop");
+			Assert.True(assc2 == null);
+		}
+
+		[Fact]
+		public void TestCycle()
+		{
+			var p = new List<string> { "a", "b" };
+			var count = 0;
+			p.Cycle(a => count++, 2);
+			Assert.True(count == 4);
+		}
+
+		[Fact]
+		public void TestForEach()
+		{
+			var counter = 0;
+			Enumerable.Range(1, 4).ForEach(a => counter++);
+			Assert.Equal(counter, 4);
+		}
+
+		[Fact]
+		public void TestForEachIndex()
+		{
+			var counter = 0;
+			Enumerable.Range(1, 4).ForEach((a, index) =>
+			{
+				Assert.Equal(counter, index);
+				counter++;
+			});
+			Assert.Equal(counter, 4);
+		}
+
+		[Fact]
+		public void TestDistinct()
+		{
+			var items = new List<Tuple<string, string>>();
+			items.Add(Tuple.Create("awesom", "1"));
+			items.Add(Tuple.Create("awesom", "7"));
+			items.Add(Tuple.Create("awesom", "6"));
+			items.Add(Tuple.Create("awzomes", "2"));
+			items.Add(Tuple.Create("pwned", "3"));
+			items.Add(Tuple.Create("kewlbz", "4"));
+			var result = items.Distinct(a => a.Item1);
+			Assert.True(result.Count() == 4);
+		}
+	}
+}
---
+++ src/LinqRb/Extensions/IEnumerable.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq
+{
+	public static class IEnumerable
+	{
+		/// <summary>
+		/// Return Enumerable where action is not true
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="source"></param>
+		/// <param name="predicate"></param>
+		/// <returns></returns>
+		public static IEnumerable<T> Reject<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+		{
+			foreach(T element in source)
+			{
+				if(!predicate(element)) yield return element;
+			}
+		}
+
+		/// <summary>
+		/// Returns the first array that contains the object
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="source"></param>
+		/// <param name="obj"></param>
+		/// <returns></returns>
+		public static IEnumerable<T> AssocFirstOrDefault<T>(this IEnumerable<IEnumerable<T>> source, T obj)
+		{
+			var outer = source.GetEnumerator();
+			while(outer.MoveNext())
+			{
+				var inner = outer.Current.GetEnumerator();
+				while(inner.MoveNext())
+				{
+					if(inner.Current.Equals(obj))
+					{
+						return outer.Current;
+					}
+				}
+			}
+			return null;
+		}
+
+		/// <summary>
+		/// Remove all nulls
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="source"></param>
+		/// <returns></returns>
+		public static IEnumerable<T> Compact<T>(this IEnumerable<T> source)
+			where T : class
+		{
+			foreach(var item in source)
+			{
+				if(item != null)
+				{
+					yield return item;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Run action over array x times
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="source"></param>
+		/// <param name="action"></param>
+		/// <param name="times">Times we should enumerate over the array</param>
+		public static void Cycle<T>(this IEnumerable<T> source, Action<T> action, int times)
+		{
+			for(int i = 0; i < times; i++)
+			{
+				Enumerate(source, action);
+			}
+		}
+
+		/// <summary>
+		/// Infinate loop over Enumerable calling the action over each one
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="source"></param>
+		/// <param name="action"></param>
+		public static void Cycle<T>(this IEnumerable<T> source, Action<T> action)
+		{
+			while(true)
+			{
+				Enumerate(source, action);
+			}
+		}
+
+		public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+		{
+			Enumerate(source, action);
+		}
+
+		public static void ForEach<T>(this IEnumerable<T> source, Action<T, int> action)
+		{
+			var index = 0;
+			var enumerator = source.GetEnumerator();
+			while(enumerator.MoveNext())
+			{
+				action?.Invoke(enumerator.Current, index);
+				index++;
+			}
+		}
+
+		public static IEnumerable<T> Distinct<T>(this IEnumerable<T> source, Func<T, object> comparer)
+		{
+			var items = new HashSet<object>();
+			foreach(var item in source)
+			{
+				var comparedItem = comparer?.Invoke(item);
+				if(!items.Contains(comparedItem))
+				{
+					items.Add(comparedItem);
+					yield return item;
+				}
+			}
+		}
+
+		private static void Enumerate<T>(IEnumerable<T> source, Action<T> action)
+		{
+			var enumerator = source.GetEnumerator();
+			while(enumerator.MoveNext())
+			{
+				action?.Invoke(enumerator.Current);
+			}
+		}
+	}
+}
